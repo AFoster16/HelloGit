5378609ab1f2d1b68bbe4b149473e948f216ea3e
andrewfoster <andrewfoster@andrews-mbp-7.unl.edu>
2022-08-25 11:56:33 -0500

    Added our majors

---
+++ src/Hack01/Hello.java
@@ -0,0 +1,10 @@
+package Hack01;
+public class Hello {
+	
+	public static void main(String args[]) {
+		System.out.println("Andy Foster!");
+		System.out.println("Will Schmidt!");
+		System.out.println("We're both computer science majors!");
+	}
+
+}

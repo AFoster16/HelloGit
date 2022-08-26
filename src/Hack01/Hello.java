3d70a37bfbf4de2b402d8e9a7199df9b247399be
willschmidt <willschmidt@localhost>
2022-08-26 15:31:48 -0500

    Added my name (Will) and major

--- src/Hack01/Hello.java
+++ src/Hack01/Hello.java
@@ -3,8 +3,10 @@ public class Hello {
 	
 	public static void main(String args[]) {
 		System.out.println("Andy Foster!");
-		System.out.println("Will Schmidt!");
+		System.out.println("Hello I am Will Schmidt");
+		System.out.println("I am a computer science major.");
 		System.out.println("We're both computer science majors!");
+		
 	}
 
 }
